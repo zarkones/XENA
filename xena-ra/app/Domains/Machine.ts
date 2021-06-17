7f66e3e69d593116e76266d671051c9730ad7280
Zarkones <zarkones.xena@gmail.com>
2021-06-17 19:33:40 +0200

    Code formated.

--- xena-ra/app/Domains/Machine.ts
+++ xena-ra/app/Domains/Machine.ts
@@ -3,17 +3,11 @@ import isDocker from 'is-docker'
 import isWSL from 'is-wsl'
 
 export default class Machine {
-  public static cpu = async () => {
-    const others = await sysinfo.cpu()
-    const cpuTemp = await sysinfo.cpuTemperature()
-    const curentSpeed = await sysinfo.cpuCurrentSpeed()
-
-    return {
-      ...others,
-      cpuTemp,
-      curentSpeed,
-    }
-  }
+  public static cpu = async () => ({
+    ...(await sysinfo.cpu()),
+    cpuTemp: await sysinfo.cpuTemperature(),
+    curentSpeed: await sysinfo.cpuCurrentSpeed(),
+  })
 
   public static battery = () => sysinfo.battery()
 
