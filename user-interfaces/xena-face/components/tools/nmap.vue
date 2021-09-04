3ad8a30cbd10934110b9b76835a9788b6343ab18
Zarkones <17931195+zarkones@users.noreply.github.com>
2021-09-04 19:54:39 +0200

    wip.

---
+++ user-interfaces/xena-face/components/tools/nmap.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card>
+    <v-card-content>
+        <v-text-field
+        dense
+        v-model = 'shellCode'
+        outlined
+        label = 'Enter a command...'
+        color = 'rgba(189, 147, 249, 1)'
+        @change = 'issueMessages'
+      >
+        Address
+      </v-text-field>
+    </v-card-content>
+  </v-card>
+</template>
--- user-interfaces/xena-face/pages/offense.vue
+++ user-interfaces/xena-face/pages/offense.vue
@@ -1,120 +1,81 @@
 <template>
   <v-card
     tile
+    flat
   >
-    <v-tabs
-      v-model = 'selectedTab'
+    <v-card-title
+      class = '
+        purple-color
+      '
     >
-      <v-tab
-        v-for = 'item in tabItems'
-        :key = 'item.tab'
-      >
-        {{ item.tab }}
-      </v-tab>
-    </v-tabs>
+      Reconnaissance
+    </v-card-title>
 
-    <v-tabs-items v-model = 'selectedTab'>
-      <v-tab-item
-        v-for = 'item in tabItems'
-        :key = 'item.tab'
+    <v-card-text>
+      Set of security tools.
+    </v-card-text>
+
+    <div class = 'ma-4'>
+      <v-expansion-panels
+        if = 'tools.length'
+        accordion
+        tile
       >
-        <v-card
-          v-if = 'item.content == `SHELL`'
-          flat
+        <v-expansion-panel
+          v-for = '(tool, toolIndex) in tools'
+          :key = 'toolIndex'
         >
-          <v-card-text
-            v-if = '!clients.length'
-          >
-            Please, select at least one client.
-          </v-card-text>
+          <v-expansion-panel-header>
+            {{ tool.name }}
+          </v-expansion-panel-header>
 
-          <!--
-            We should ask the user to select clients.
-          -->
-          <div
-            v-if = 'clients.length'
-            class = '
-              ml-4
-              mr-4
-              mt-4
-            '
-          >
-            <div
-              v-for = 'message in messages'
-              :key = 'message.id'
-            >
-              <MessageDisplay
-                :message = 'message'
-              />
-            </div>
-
-            <Shell
-              :clients = 'clients'
+          <v-expansion-panel-content>
+            <Nmap
+              v-if = 'tool.name == "Nmap"'
             />
-          </div>
-        </v-card>
-
-        <v-card
-          v-if = 'item.content == `SYSTEM_MONITOR`'
-          flat
-        >
-          Feature is disabled. Development is progressing. Consider opening a pull-request for the implementation of system monitoring.
-        </v-card>
-      </v-tab-item>
-    </v-tabs-items>
-
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
   </v-card>
 </template>
 
 
 <script lang = 'ts'>
 import Vue from 'vue'
-import EventBus from '@/src/EventBus'
 
-import MessageDisplay from '@/components/interaction/message-display.vue'
-import Shell from '@/components/interaction/shell.vue'
+import Nmap from '@/components/tools/nmap.vue'
+
+import EventBus from '@/src/EventBus'
 
 import * as Service from '@/src/services'
 
 export default Vue.extend({
   components: {
-    MessageDisplay,
-    Shell,
+    Nmap,
   },
 
   data: () => ({
-    selectedTab: 0,
-    tabItems: [
-      { tab: 'Scope', content: 'SCOPE' },
-      { tab: 'History', content: 'HISTORY' },
-      { tab: 'Settings', content: 'SETTINGS' },
-    ],
+    tools: [
+      {
+        name: 'Nmap',
+        description: 'Network Mapper.',
+      },
+      {
+        name: 'Sublist3r',
+        description: 'Passive subdomain mapper.',
+      },
+      {
+        name: 'Subdomain Brute-Forcer',
+        description: 'Enumerate reachable domains with worldlist.',
+      },
+    ] as const
   }),
 
   methods: {
-    interactionDialog () {
-      this.enabled = !this.enabled
-    },
-
-    selectTab (tabName: string) {
-      this.selectedTab = tabName
-    },
-
-    async fetchMessagesWithReplies () {
-
-    },
   },
 
   mounted () {
-    EventBus.$on('interactionDialogUpdateClients', (clients: any[]) => this.clients = clients)
-
-    EventBus.$on('interactionDialogUpdateSelectedClient', async (clientId: string) => {
-      this.selectedClient = this.clients.filter(client => client.id == clientId)[0]
-      const messages = await Service.Atila.fetchMessages(this.$axios, this.selectedClient.id, true)
-      this.messages = messages.length
-        ? messages.map(message => ({ ...message, content: Buffer.from(message.content, 'base64') }))
-        : []
-    })
   },
 })
 </script>
