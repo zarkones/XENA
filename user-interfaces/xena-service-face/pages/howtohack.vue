2c6c2bc3a984bc82e1d801d451cdac9448ef82ae
Zarkones <17931195+zarkones@users.noreply.github.com>
2021-10-13 22:10:18 +0200

    Initial text.

---
+++ user-interfaces/xena-service-face/pages/howtohack.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class = '
+      ma-4
+    '
+  >
+    HTTP Smuggling
+    Web Cache Poisoning
+    Request Forgery
+    Cross-Origin Resource Sharing Misconfiguration
+    Missing or Wrong Implementation of Anti Cross-Site Request Forgery Token
+    SQL Injections
+    Account Takeover
+    Command Injection
+    Weak Credentials
+    Weak Rate-Limiting
+    Self-Inflicting Denial of Service
+    Abuse of the “forgot a password” feature
+    Web Cache Deception
+    Weak Encryption
+    Cross-Site Scripting
+    Weak Input Escaping and Validation
+  </div>
+</template>
