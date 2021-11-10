170740fb008c17aec108f74650ec16d8eedf02a8
Zarkones <17931195+zarkones@users.noreply.github.com>
2021-11-10 23:05:22 +0100

    Lucid initialized in Rack.

--- services/xena-service-rack/env.ts
+++ services/xena-service-rack/env.ts
@@ -21,4 +21,10 @@ export default Env.rules({
 	APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
+
+	PG_HOST: Env.schema.string({ format: 'host' }),
+	PG_PORT: Env.schema.number(),
+	PG_USER: Env.schema.string(),
+	PG_PASSWORD: Env.schema.string.optional(),
+	PG_DB_NAME: Env.schema.string(),
 })
