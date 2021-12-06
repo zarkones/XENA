557b5592ae5fb1718f908af645532dc9a5f9826e
Zarkones <17931195+zarkones@users.noreply.github.com>
2021-12-06 15:26:02 +0100

    Initial commit of domena.

---
+++ services/xena-service-domena/config/database.ts
@@ -0,0 +1,135 @@
+/**
+ * Config source: https://git.io/JesV9
+ *
+ * Feel free to let us know via PR, if you find something broken in this config
+ * file.
+ */
+
+import Env from '@ioc:Adonis/Core/Env'
+import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+
+const databaseConfig: DatabaseConfig = {
+  /*
+  |--------------------------------------------------------------------------
+  | Connection
+  |--------------------------------------------------------------------------
+  |
+  | The primary connection for making database queries across the application
+  | You can use any key from the `connections` object defined in this same
+  | file.
+  |
+  */
+  connection: Env.get('DB_CONNECTION'),
+
+  connections: {
+    /*
+    |--------------------------------------------------------------------------
+    | MySQL config
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for MySQL database. Make sure to install the driver
+    | from npm when using this connection
+    |
+    | npm i mysql
+    |
+    */
+    mysql: {
+      client: 'mysql',
+      connection: {
+        host: Env.get('MYSQL_HOST'),
+        port: Env.get('MYSQL_PORT'),
+        user: Env.get('MYSQL_USER'),
+        password: Env.get('MYSQL_PASSWORD', ''),
+        database: Env.get('MYSQL_DB_NAME'),
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      healthCheck: false,
+      debug: false,
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | PostgreSQL config
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for PostgreSQL database. Make sure to install the driver
+    | from npm when using this connection
+    |
+    | npm i pg
+    |
+    */
+    pg: {
+      client: 'pg',
+      connection: {
+        host: Env.get('PG_HOST'),
+        port: Env.get('PG_PORT'),
+        user: Env.get('PG_USER'),
+        password: Env.get('PG_PASSWORD', ''),
+        database: Env.get('PG_DB_NAME'),
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      healthCheck: false,
+      debug: false,
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | OracleDB config
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for Oracle database. Make sure to install the driver
+    | from npm when using this connection
+    |
+    | npm i oracledb
+    |
+    */
+    oracle: {
+      client: 'oracledb',
+      connection: {
+        host: Env.get('ORACLE_HOST'),
+        port: Env.get('ORACLE_PORT'),
+        user: Env.get('ORACLE_USER'),
+        password: Env.get('ORACLE_PASSWORD', ''),
+        database: Env.get('ORACLE_DB_NAME'),
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      healthCheck: false,
+      debug: false,
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | MSSQL config
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for MSSQL database. Make sure to install the driver
+    | from npm when using this connection
+    |
+    | npm i tedious
+    |
+    */
+    mssql: {
+      client: 'mssql',
+      connection: {
+        user: Env.get('MSSQL_USER'),
+        port: Env.get('MSSQL_PORT'),
+        server: Env.get('MSSQL_SERVER'),
+        password: Env.get('MSSQL_PASSWORD', ''),
+        database: Env.get('MSSQL_DB_NAME'),
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      healthCheck: false,
+      debug: false,
+    }
+  }
+}
+
+export default databaseConfig
