2b076826d8da2db86f272ff636d63a4b02ef4f25
= <zarkones.xena@gmail.com>
2025-03-15 21:53:51 +0100

    version change

--- ui/pages/settings.go
+++ ui/pages/settings.go
@@ -42,7 +42,7 @@ func Settings() fyne.CanvasObject {
 			widget.NewLabel("Authentication Token:"),
 			authToken,
 			layout.NewSpacer(),
-			widget.NewLabel("XENA Beta v0.2"),
+			widget.NewLabel("XENA (Beta) v0.3.0"),
 		),
 	)
 }
